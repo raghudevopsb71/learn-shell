11a2adf6389f4ce42212f679c8a48a8c60b4b60e
Raghu K <raghuk.vit@gmail.com>
2023-02-17 08:38:02 +0530

    Update learn-shell/

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,12 +1,12 @@
 # 0-n , $*/$@, $#
 
-echo $1 # First Argument
-echo $2 # Second Argument
+echo First Argument $1 # First Argument
+echo Second Argument $2 # Second Argument
 
-echo $0 # Script Name
+echo Script Name $0 # Script Name
 
-echo $*
-echo $@ # All Arguments
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
 
-echo $# # Number of Arguments
+echo Number of Arguments = $# # Number of Arguments
 
